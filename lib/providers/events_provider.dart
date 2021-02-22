c7e86eb9532e8195169cb114bf713e821ded5892
raunak_k02 <raunakk728@gmail.com>
2021-02-23 02:40:18 +0530

    created launcher icon and added flutter_local_notifications

--- lib/providers/events_provider.dart
+++ lib/providers/events_provider.dart
@@ -1,7 +1,13 @@
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 
 import '../models/event.dart';
+
+void trigger(int id) {
+  id += 1;
+  print('Weekday Alaram Triggered');
+}
 
 class EventsProvider with ChangeNotifier {
   List<Event> _events = [];
@@ -12,7 +18,24 @@ class EventsProvider with ChangeNotifier {
 
   void addEvent(Event e) {
     _events.add(e);
-    Hive.box('events').add(e);
+    Hive.box('events').add(e).then((_) {
+      var now = DateTime.now();
+      print('Initializing weekDay alarm !!!!! LOL');
+      AndroidAlarmManager.periodic(
+        Duration(minutes: 2),
+        0,
+        trigger,
+        rescheduleOnReboot: true,
+        startAt: DateTime(
+          now.year,
+          now.month,
+          now.day,
+          e.startTime.hour,
+          e.startTime.minute,
+        ),
+        wakeup: true,
+      );
+    });
     notifyListeners();
   }
 
