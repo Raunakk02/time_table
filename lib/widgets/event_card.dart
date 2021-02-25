99a56f90649e47366ecd7f829d3451c3a0641bad
raunak_k02 <raunakk728@gmail.com>
2021-02-25 18:04:55 +0530

    remove onTap on EventCard and added app signing info

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -19,57 +19,48 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context)
-            .pushNamed(EventDetailsScreen.routeName, arguments: {
-          'weekDayEvent': weekDayEvent,
-          'weekDayText': weekDayText,
-        });
-      },
-      child: Card(
-        color: cardColor,
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 2, horizontal: 0),
-          child: ListTile(
-            tileColor: Colors.black54,
-            leading: Container(
-              padding: EdgeInsets.all(2),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [Colors.pink, Colors.orange],
-                ),
+    return Card(
+      color: cardColor,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 2, horizontal: 0),
+        child: ListTile(
+          tileColor: Colors.black54,
+          leading: Container(
+            padding: EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.pink, Colors.orange],
               ),
-              width: 80,
-              height: 80,
-              child: FittedBox(
-                child: Text(
-                  '${weekDayEvent.startTime.format(context)}\n|\n${weekDayEvent.endTime.format(context)}',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                  ),
+            ),
+            width: 80,
+            height: 80,
+            child: FittedBox(
+              child: Text(
+                '${weekDayEvent.startTime.format(context)}\n|\n${weekDayEvent.endTime.format(context)}',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
                 ),
               ),
             ),
-            title: Text(
-              weekDayEvent.title,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(color: Colors.white),
-            ),
-            subtitle: Text(
-              weekDayEvent.description,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(color: Colors.white),
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () => deleteEvent(weekDayEvent),
-            ),
+          ),
+          title: Text(
+            weekDayEvent.title,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(color: Colors.white),
+          ),
+          subtitle: Text(
+            weekDayEvent.description,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(color: Colors.white),
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () => deleteEvent(weekDayEvent),
           ),
         ),
       ),
